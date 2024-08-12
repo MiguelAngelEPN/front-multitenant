34fdd4b85da895e8e6fc81afb3e836f89fafebf5
MiguelMuenala <mickaelo.1755@gmail.com>
2024-08-12 11:51:59 -0500

    cambio de estilos es formulario por dropdown
    cambio de estilos es formulario por dropdown

--- proyect/app/companies/[idtenant]/[idEmployee]/[IdTask]/createKPIForm/form-dropdown/page.jsx
+++ proyect/app/companies/[idtenant]/[idEmployee]/[IdTask]/createKPIForm/form-dropdown/page.jsx
@@ -63,75 +63,75 @@ export default function CreateKpibyDropdown() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center homepage">
+        <div className="min-h-screen flex items-center justify-center bg-[--complementary-color] p-6 homepage">
             <Link href={`/companies/${params.idtenant}/${params.idEmployee}/${params.IdTask}/createKPIForm`} className="absolute top-4 left-4 bg-[--secondary-color] hover:bg-[--primary-color] text-white font-semibold py-2 px-4 rounded-full shadow-md transition-all">
                 ⬅️ Back
             </Link>
 
-            <div className="bg-[--primary-color] backdrop-blur-md shadow-lg rounded-lg p-8 w-full max-w-lg">
-                <h1 className="text-3xl font-semibold mb-6 text-center text-white">Crear KPI para Tarea con evaluación por dropdown</h1>
+            <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg border-2 border-[--primary-color]">
+                <h1 className="text-3xl font-semibold mb-6 text-center text-[--primary-color]">Crear KPI para Tarea con evaluación por dropdown</h1>
                 <form onSubmit={handleSubmit} className='text-black'>
                     <div className="mb-4">
-                        <label className="block text-sm font-medium text-[--complementary-color] mb-2">Título del KPI</label>
+                        <label className="block text-sm font-medium text-[--secondary-color] mb-2">Título del KPI</label>
                         <input
                             type="text"
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
                             required
-                            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+                            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-sm font-medium text-[--complementary-color] mb-2">Objetivo (Target)</label>
+                        <label className="block text-sm font-medium text-[--secondary-color] mb-2">Objetivo (Target)</label>
                         <input
                             type="number"
                             value={target}
                             onChange={(e) => setTarget(e.target.value)}
                             required
-                            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+                            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
                         />
                     </div>
                     <div className="mb-6">
-                        <p className='block text-m font-medium text-[--complementary-color] mb-2'>Rango de fechas:</p>
+                        <p className='block text-m font-medium text-[--secondary-color] mb-2'>Rango de fechas:</p>
                         <div className='w-full flex justify-between'>
                             <div className='w-[220px]'>
-                                <p className='block text-sm font-medium text-[--complementary-color] mb-2'>Fecha de inicio:</p>
+                                <p className='block text-sm font-medium text-[--secondary-color] mb-2'>Fecha de inicio:</p>
                                 <input
                                     type="datetime-local"
                                     value={startDate}
                                     onChange={(e) => setStartDate(e.target.value)}
                                     required
-                                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+                                    className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
                                 />
                             </div>
                             <div className='w-[220px]'>
-                                <p className='block text-sm font-medium text-[--complementary-color] mb-2'>Fecha de fin:</p>
+                                <p className='block text-sm font-medium text-[--secondary-color] mb-2'>Fecha de fin:</p>
                                 <input
                                     type="datetime-local"
                                     value={endDate}
                                     onChange={(e) => setEndDate(e.target.value)}
                                     required
-                                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+                                    className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
                                 />
                             </div>
                         </div>
                     </div>
 
                     <div className="mb-6">
-                        <h2 className="text-lg font-semibold mb-4 text-[--complementary-color]">Evaluación por dropdown</h2>
+                        <h2 className="text-lg font-semibold mb-4 text-[--primary-color]">Evaluación por dropdown</h2>
                         <div className="mb-4">
                             <input
                                 type="text"
                                 value={newCriterion}
                                 onChange={(e) => setNewCriterion(e.target.value)}
                                 placeholder="Ingrese un criterio"
-                                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+                                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
                             />
                             <div className="flex justify-end mt-2">
                                 <button
                                     type="button"
                                     onClick={handleAddCriterion}
-                                    className="px-4 py-2 text-sm text-white bg-green-500 rounded-full hover:bg-green-600 border-2"
+                                    className="px-4 py-2 text-sm text-white bg-[--secondary-color] rounded-full hover:bg-[--primary-color] transition-all"
                                 >
                                     Añadir Criterio
                                 </button>
@@ -139,24 +139,23 @@ export default function CreateKpibyDropdown() {
                         </div>
                         <ul className="list-disc pl-5">
                             {dropdownCriteria.map((criterion, index) => (
-                                <li key={index} className="bg-[--complementary-color] text-[--thertiary-color] flex items-center justify-between border-black border-2 p-1 my-2">
+                                <li key={index} className="bg-[--tertiary-color] text-white flex items-center justify-between p-2 my-2 rounded">
                                     {criterion}
                                     <button
                                         type="button"
                                         onClick={() => handleRemoveCriterion(index)}
-                                        className="ml-2 text-red-600 hover:text-red-800 focus:outline-none"
+                                        className="ml-2 text-red-600 hover:text-red-800 transition-all"
                                     >
                                         ✖
                                     </button>
                                 </li>
                             ))}
                         </ul>
-
                     </div>
 
                     <button
                         type="submit"
-                        className="w-full px-6 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-full hover:bg-indigo-700 border-2"
+                        className="w-full py-3 px-4 bg-[--primary-color] text-white rounded-full hover:bg-[--secondary-color] focus:outline-none focus:ring-2 focus:ring-[--primary-color] transition-all"
                     >
                         Crear KPI
                     </button>
